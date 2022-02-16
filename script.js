7c117bd5b3fb2f4e304a094e2c2bb850d9d0c31d
saran chakravarthi <chakravarthisaran7@gmail.com>
2022-02-16 13:08:16 +0530

    changed description, scrollbar and copy-btn

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
-// navbar
-const hamBurgerIcons = document.getElementsByClassName("hamburger-icon");
-for(let i= 0; i < hamburgerIcons.length; i++){
-    hamBurgerIcons[i].addEventListener("click", function(){
-        navLinksContainer.classList.toggle("open");
-        if(navLinksContainer.classList.contains("open")){
-            hamBurgerIcons[i].innerText = "close"
-        }else{
-            hamBurgerIcons[i].innerText = "menu"
-        }
-    })
+// dropdown
+
+let dropDown = document.getElementsByClassName("menu-dropdown");
+let asideList = document.getElementsByClassName("aside-menu");
+let dropDownIcon = document.getElementsByClassName("dd-menu-icon");
+
+dropDown[0].addEventListener("click", () => {
+  asideList[0].classList.toggle("hidden");
+  if (!asideList[0].classList.contains("hidden")) {
+    dropDownIcon[0].style.transform = "rotate(180deg)";
+  } else {
+    dropDownIcon[0].style.transform = "rotate(0deg)";
+  }
+});
+
+// copy button
+
+let copyButtons = document.getElementsByClassName("copy-btn");
+
+function copyBtnChange() {
+  for (let i = 0; i < copyButtons.length; i++) {
+    copyButtons[i].classList.add("btn", "btn-primary");
+    copyButtons[i].style.fontSize = "1.6rem";
+    copyButtons[i].style.textAlign = "center";
+
+    let copiedBtn = document.createElement("button");
+    copiedBtn.style.display = "none";
+    copiedBtn.classList.add("btn");
+    copiedBtn.style.backgroundColor = "#188464";
+    copiedBtn.style.fontWeight = "bold";
+    copiedBtn.innerHTML = "Copied";
+    copyButtons[i].parentNode.insertBefore(copiedBtn, copyButtons[i]);
+    copyButtons[i].addEventListener("click", function () {
+      this.style.display = "block !important";
+      copiedBtn.style.display = "block";
+
+      this.setAttribute("style", "display: none !important");
+
+      setTimeout(() => {
+        copyButtons[i].setAttribute("style", "display: block !important");
+        copyButtons[i].style.fontSize = "1.6rem";
+        copyButtons[i].style.textAlign = "center";
+        copiedBtn.style.display = "none";
+      }, 1000);
+    });
+  }
 }
